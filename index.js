531e5ff6c133322210a306319204fb4599490586
Jikyubin <rbqls0502@gmail.com>
2020-07-13 18:57:40 +0900

    셈틀꾼 서버 예시 코드

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+
+app.use((req,res,next)=>{
+    console.log("new request",req.method, req.path, new Date().toLocaleDateString());
+    next();
+})
+const calcRouter = require('./router/calc');
+app.use('/calc',calcRouter);
+
+app.listen(80, function(){
+    console.log("App is running on port 80");
+});
