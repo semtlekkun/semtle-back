b69f46b18c9764b682e5ed1c7ced62c7bb60880c
Jikyubin <rbqls0502@gmail.com>
2020-07-15 04:34:32 +0900

    디비접속 코드 추가

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+var connect = require('./schemas');
+connect();
 
 app.use(bodyParser.json());
 
