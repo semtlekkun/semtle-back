0f360a221f17efb980da68fada127699712f3cc6
youngmin1006 <beanpole_1006@naver.com>
2020-08-08 19:36:16 +0900

    로그인 수정

--- router/log.js
+++ router/log.js
@@ -10,10 +10,9 @@
      var input_id = req.body._id;
      var input_pw = req.body.pw;
      if (typeof input_id != 'number'){
-        Admin.find({},{_id:input_id , pw:input_pw})//문자열(관계자일때)
+        Admin.find({},{"_id":input_id , "pw":input_pw})//문자열(관계자일때)
         .then((_id,pw)=>{
-            console.log(input_id,_id)
-            if((input_id === _id) &&(input_pw === pw)){
+            if((input_id === _id) &&(input_pw === pw)){//-->다 wrong으로 뜬다. _id가 배열이기 때문
                 res.json({status:"success",_id:_id});
             }
             else{
@@ -27,20 +26,21 @@
         })
      }     
      else if(typeof input_id ==='number'){//number
-         Student.find({},{_id:input_id , pw:input_pw})
-         .then((_id,pw)=>{//오류
-            console.log(input_id,_id)
-            if((input_id === _id) &&(input_pw === pw)){
+        Student.find({},{"_id":input_id , "pw":input_pw})//문자열(관계자일때)
+        .then((_id,pw)=>{
+            if(_id.length>0){//--->다 success로 뜬다.(수정 필요)
                 res.json({status:"success",_id:_id});
             }
             else{
                 res.json({status:"wrong"});
             }
+            
         })
         .catch((err)=>{
             console.log(err);
             res.json({status:"error"});
         })
+         
      }
     
  });
