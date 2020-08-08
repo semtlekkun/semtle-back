4b9a405cccad4bd0c4173d38e7eb6cf236a49edf
youngmin1006 <beanpole_1006@naver.com>
2020-08-08 19:57:53 +0900

    로그인

--- router/log.js
+++ router/log.js
@@ -7,12 +7,12 @@
  var admincheck = "ADMIN" || "admin";
 
  router.post('/in',(req,res)=>{
-     var input_id = req.body._id;
-     var input_pw = req.body.pw;
-     if (typeof input_id != 'number'){
-        Admin.find({},{"_id":input_id , "pw":input_pw})//문자열(관계자일때)
+    var _id = req.body._id;
+    var pw = req.body.pw;
+     if (typeof _id != 'number'){
+        Admin.find({},{"_id":_id , "pw":pw})//문자열(관계자일때)
         .then((_id,pw)=>{
-            if((input_id === _id) &&(input_pw === pw)){//-->다 wrong으로 뜬다. _id가 배열이기 때문
+            if(_id.length>0){
                 res.json({status:"success",_id:_id});
             }
             else{
@@ -26,9 +26,9 @@
         })
      }     
      else if(typeof input_id ==='number'){//number
-        Student.find({},{"_id":input_id , "pw":input_pw})//문자열(관계자일때)
+        Student.find({},{_id:_id ,pw:pw})//학번
         .then((_id,pw)=>{
-            if(_id.length>0){//--->다 success로 뜬다.(수정 필요)
+            if(_id.length>0){
                 res.json({status:"success",_id:_id});
             }
             else{
