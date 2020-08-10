d0fc5feb98380edaf8a8ead22df16fe4d01589d5
Jikyubin <rbqls0502@gmail.com>
2020-08-10 23:24:38 +0900

    파일 업로드

---
+++ router/middlewares/adminConfirmation.js
@@ -0,0 +1,4 @@
+module.exports.adminConfirmation = function(req,res,next){
+    if(res.locals.isAdmin) next();
+    else res.json({status:"notAdmin"});
+}
---
+++ router/middlewares/findWriter.js
@@ -0,0 +1,26 @@
+const Student = require('../../schemas/student');
+const Admin = require('../../schemas/admin');
+module.exports.findWriter = function (req, res, next) {
+    if (res.locals.isAdmin) {
+        Admin.findOne({ _id: res.locals.id }, { nick: true })
+            .then((admin) => {
+                res.locals.writer = admin.nick;
+                next();
+            })
+            .catch((err) => {
+                console.log(err);
+                res.json({ status: "error" });
+            })
+    }
+    else {
+        Student.findOne({ _id: res.locals.id }, { nick: true })
+            .then((student) => {
+                res.locals.writer = student.nick;
+                next();
+            })
+            .catch((err) => {
+                console.log(err);
+                res.json({ status: "error" });
+            })
+    }
+}
