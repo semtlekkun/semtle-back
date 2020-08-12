f3e81dded0d8d89c9d2154d344001634e2cc16be
wooseok jang <mmyu2090@gmail.com>
2020-08-13 06:11:50 +0900

    wooseok2 init

--- router/index.js
+++ router/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const {verifyToken} = require("./middlewares/authorization");
-const {adminConfirmation} =  require('./middlewares/adminConfirmation');
+const { verifyToken } = require("./middlewares/authorization");
+const { adminConfirmation } = require('./middlewares/adminConfirmation');
 
 const loginRouter = require('./log');
-router.use('/log',loginRouter);
+router.use('/log', loginRouter);
 
 const recruitRouter = require('./recruit');
 router.use('/recruit', recruitRouter);
@@ -14,18 +14,21 @@ const noticeRouter = require('./notice');
 router.use('/notice', noticeRouter);
 
 const portfolioRouter = require("./pf");
-router.use('/pf',portfolioRouter);
+router.use('/pf', portfolioRouter);
 
 const studentRouter = require("./student");
-router.use('/student',verifyToken,adminConfirmation,studentRouter);
+router.use('/student', verifyToken, adminConfirmation, studentRouter);
 
 const managementRouter = require('./management');
-router.use('/management',managementRouter);
+router.use('/management', managementRouter);
 
 const questionRouter = require('./question');
 router.use('/question', questionRouter);
 
 const answerRouter = require('./answer');
 router.use('/answer', answerRouter);
+
+//const imageRouter = require('./image');
+//router.use('/image', imageRouter);
 
 module.exports = router;
