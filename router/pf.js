8990da1e69601bf6d5f3adbd42c4bae806e95ac2
Jikyubin <rbqls0502@gmail.com>
2020-08-14 00:24:50 +0900

    오타수정

--- router/pf.js
+++ router/pf.js
@@ -86,7 +86,7 @@ router.post("/input",verifyToken,findWriter,upload.array('projectImages'),(req,r
                 contents:req.body.contents,
                 link:req.body.link != undefined? req.body.link:null,
                 projectStartDate:req.body.projectStartDate,
-                prijectEndDate:req.body.prijectEndDate,
+                projectEndDate:req.body.prijectEndDate,
                 projectTeamName:req.body.projectTeamName,
                 teamLeaderCode:req.body.teamLeaderCode,
                 projectImages:req.files.map((image)=>{return image.filename}),
