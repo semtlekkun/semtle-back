d71f9b3b7df7750bece145f8f73c401f597ef152
yooncastle <dbstjd427@gmail.com>
2020-08-14 20:00:29 +0900

    개인정보 암호화

--- router/student.js
+++ router/student.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const crypto = require("crypto");
 const Student = require('../schemas/student');
 const createNick = require('./controllers/user.controller').createNick;
 
@@ -11,74 +12,87 @@ const createNick = require('./controllers/user.controller').createNick;
 // 전화번호, 이메일 등은 암호화 후 저장해야 함 (추가예정)
 // 에러 핸들러를 만들어야 함
 
-router.get('/list/:page',(req,res)=>{
+router.get('/list/:page', (req, res) => {
     const page = req.params.page;
     Student.find({}).count()
-    .then((count)=>{
-        Student.find({},{_id:1,phoneNum:1,name:1})
-        .sort({ _id: -1 })
-        .skip((page - 1) * 10)
-        .limit(10)
-        .then((students)=>{
-            res.status(200).json({ status:"success",students: students,count:count });
+        .then((count) => {
+            Student.find({}, { _id: 1, phoneNum: 1, name: 1 })
+                .sort({ _id: -1 })
+                .skip((page - 1) * 10)
+                .limit(10)
+                .then((students) => {
+                    console.log(students[8].phoneNum);
+                    const decipher = crypto.createDecipher('aes-256-cbc', 'yooncastle');
+                    let result2 = decipher.update(students[8].phoneNum, 'base64', 'utf8'); // 암호화할문 (base64, ut
+                    result2 += decipher.final('utf8'); // 암호화할문장 (여기도 base64대신 utf8)
+                    students[8].phoneNum = result2;
+                    console.log(students[8].phoneNum);
+                    // for (let i = 0; i < students.length; i++) {
+                    //     //console.log(students[i].phoneNum);
+                    //     // const decipher = crypto.createDecipher('aes-256-cbc', 'yooncastle');
+                    //     // let result2 = decipher.update(students[i].phoneNum, 'base64', 'utf8'); // 암호화할문 (base64, utf8이 위의 cipher과 반대 순서입니다.)
+                    //     // result2 += decipher.final('utf8'); // 암호화할문장 (여기도 base64대신 utf8)
+                    //     // students[i].phoneNum = result2;
+                    // }
+                    res.status(200).json({ status: "success", students: students, count: count });
+                })
+                .catch((err) => {
+                    console.log(err);
+                    res.status(500).json({ status: "error" });
+                })
         })
-        .catch((err)=>{
+        .catch((err) => {
             console.log(err);
-            res.status(500).json({status:"error"});
+            res.status(500).json({ status: "error" });
         })
-    })
-    .catch((err)=>{
-        console.log(err);
-        res.status(500).json({status:"error"});
-    })
 });
 
 // 닉네임도 바뀐 이름에 맞게 수정해야함: 완료
-router.put('/update',(req,res)=>{
-    Student.update({_id:req.body.studentCode},
-        { $set: {name:req.body.name, nick:createNick(req.body.studentCode,req.body.name) ,phoneNum:req.body.phoneNum }})
-    .then((result)=>{
-        console.log(result);
-        if(result.n) res.status(200).json({status:"success"});
-        else res.status(400).json({status:"noMatched"});
-    })
-    .catch((err)=>{
-        console.log(err);
-        res.status(500).json({status:"error"});
-    })
+router.put('/update', (req, res) => {
+    Student.update({ _id: req.body.studentCode },
+        { $set: { name: req.body.name, nick: createNick(req.body.studentCode, req.body.name), phoneNum: req.body.phoneNum } })
+        .then((result) => {
+            console.log(result);
+            if (result.n) res.status(200).json({ status: "success" });
+            else res.status(400).json({ status: "noMatched" });
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json({ status: "error" });
+        })
 });
 
-router.post('/input',(req,res)=>{
+router.post('/input', (req, res) => {
     const student = new Student({
-        _id:req.body.studentCode,
-        pw:req.body.studentCode,
-        name:req.body.name,
-        nick:createNick(req.body.studentCode,req.body.name),
-        phoneNum:req.body.phoneNum,
-        image:"default.jpg"
+        _id: req.body.studentCode,
+        pw: req.body.studentCode,
+        name: req.body.name,
+        nick: createNick(req.body.studentCode, req.body.name),
+        phoneNum: req.body.phoneNum,
+        image: "default.jpg"
     });
     student.save()
-    .then(()=>{
-        res.status(200).json({status:"success"});
-    })
-    .catch((err)=>{
-        console.log(err);
-        if(err.keyValue._id) res.status(400).json({status:"duplicate"});
-        else res.status(500).json({status:"error"});
-    });
+        .then(() => {
+            res.status(200).json({ status: "success" });
+        })
+        .catch((err) => {
+            console.log(err);
+            if (err.keyValue._id) res.status(400).json({ status: "duplicate" });
+            else res.status(500).json({ status: "error" });
+        });
 })
 
 // 관리자 확인이 필요
-router.delete('/delete',(req,res)=>{
-    Student.remove({_id:req.body.studentCode})
-    .then((result)=>{
-        if(result.deletedCount) res.status(200).json({status:"success"})
-        else res.status(400).json({status:"none"});
-    })
-    .catch(err=>{
-        console.log(err);
-        res.status(500).json({status:"error"});
-    })
+router.delete('/delete', (req, res) => {
+    Student.remove({ _id: req.body.studentCode })
+        .then((result) => {
+            if (result.deletedCount) res.status(200).json({ status: "success" })
+            else res.status(400).json({ status: "none" });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ status: "error" });
+        })
 })
 
 module.exports = router;
--- schemas/student.js
+++ schemas/student.js
@@ -2,11 +2,12 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 const saltRounds = require("../config/hash").saltRounds;
 const bcrypt = require("bcrypt");
+const crypto = require("crypto");
 const studentSchema = new Schema({
     //_id: Schema.Types.ObjectId, //따로 선언하지 않아도 자동으로 set 
-    _id:{
-        type:String,
-        required:true
+    _id: {
+        type: String,
+        required: true
     },
     pw: {
         type: String,
@@ -30,17 +31,33 @@ const studentSchema = new Schema({
     }
 }, { versionKey: false }); //"__v"필드를 안보이게 하려고 
 
-studentSchema.pre('save',function(next){
+studentSchema.pre('save', function (next) {
     const user = this;
+    const cipher = crypto.createCipher('aes-256-cbc', 'yooncastle');
+    let result = cipher.update(user.phoneNum, 'utf8', 'base64'); // 'HbMtmFdroLU0arLpMflQ'
+    result += cipher.final('base64'); // 'HbMtmFdroLU0arLpMflQYtt8xEf4lrPn5tX5k+a8Nzw='
+    user.phoneNum = result;
+
     bcrypt.hash(user.pw, saltRounds, function (err, hash) {
         // Store hash in your password DB.
-        if (err){
+        if (err) {
             console.log(err);
-            res.json({status:"error"});
+            res.json({ status: "error" });
         }
         user.pw = hash;
         next();
     });
 })
+
+// studentSchema.pre('find', function (next) {
+//     const user2 = this;
+//     console.log(this._id);
+//     // const decipher = crypto.createDecipher('aes-256-cbc', 'yooncastle');
+//     // let result2 = decipher.update(user.phoneNum, 'base64', 'utf8'); // 암호화할문 (base64, utf8이 위의 cipher과 반대 순서입니다.)
+//     // result2 += decipher.final('utf8'); // 암호화할문장 (여기도 base64대신 utf8)
+//     // user.phoneNum = result2;
+//     next();
+// });
+
 
 module.exports = mongoose.model('Student', studentSchema, 'student');
