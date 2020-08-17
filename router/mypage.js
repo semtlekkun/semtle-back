ae6ca37bdbc9f67a92fba31a7cd73713f4d3e794
Jikyubin <jikyubin72@gmail.com>
2020-08-18 04:29:41 +0900

    no message

--- router/mypage.js
+++ router/mypage.js
@@ -15,7 +15,6 @@ router.get('/', verifyToken, (req, res) => {
             Portfolio.find({ students: res.locals.id })
                 .then(pfList => {
                     student.pfList = pfList == null? undefined:pfList;
-                    console.log(pfList);
                     res.json({ status: "success", student: student })
                 })
                 .catch(err => {
