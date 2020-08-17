c63e56016e62dc2c6ac36d1afda8adcb90292219
yooncastle <dbstjd427@gmail.com>
2020-08-17 20:05:46 +0900

    test

--- router/mypage.js
+++ router/mypage.js
@@ -2,30 +2,31 @@ const express = require('express');
 const router = express.Router();
 const Student = require('../schemas/student');
 const { verifyToken } = require('./middlewares/authorization');
-const {compare} = require('./middlewares/compare');
+const { compare } = require('./middlewares/compare');
 const imageUploader = require('./controllers/image.controller').imageUpload;
 
-router.get('/',verifyToken,(req,res)=>{
-    Student.findOne({_id:res.locals.id},{pw:0})
-    .then(student=>{res.json({status:"success",student:student})})
-    .catch(err=>{
-        console.log(err);
-        res.status(500).json({status:"error"});
-    })
+router.get('/', verifyToken, (req, res) => {
+    Student.findOne({ _id: res.locals.id }, { pw: 0 })
+        .then(student => { res.json({ status: "success", student: student }) })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ status: "error" });
+        })
 })
 
 router.put('/picture/update', verifyToken, imageUploader("images/students").single("image"), (req, res) => {
 
     Student.findOneAndUpdate({ _id: res.locals.id }, {
-        $set: { image: req.file != undefined? req.file.filename:"default.jpg" }
+        $set: { image: req.file != undefined ? req.file.filename : "default.jpg" }
     }, { projection: { pw: false }, new: true })
         .exec().then((student) => {
             console.log(student.image);
-            res.json({ status: "success"});
+            res.json({ status: "success" });
         })
         .catch(err => {
             console.log(err);
-            res.status(500).json({ status: "error" })});
+            res.status(500).json({ status: "error" })
+        });
 });
 
 router.put('/phoneNum/update', verifyToken, (req, res) => {
@@ -34,14 +35,19 @@ router.put('/phoneNum/update', verifyToken, (req, res) => {
         $set: { phoneNum: req.body.phoneNum }
     }, { projection: { pw: false } })
         .then(() => {
-            res.json({ status: "success"});
+            res.json({ status: "success" });
         })
         .catch(err => {
             console.log(err);
-            res.status(500).json(err)});
+            res.status(500).json(err)
+        });
 });
 
-router.put('/pw/update', verifyToken,compare, (req, res) => {
+
+
+
+
+router.put('/pw/update', verifyToken, compare, (req, res) => {
     Student.update({ _id: res.locals.id }, {
         $set: { pw: req.body.changePW }
     }).then(() => {
@@ -49,7 +55,8 @@ router.put('/pw/update', verifyToken,compare, (req, res) => {
     })
         .catch(err => {
             console.log(err);
-            res.status(500).json({ status: "error" })});
+            res.status(500).json({ status: "error" })
+        });
 });
 
 
