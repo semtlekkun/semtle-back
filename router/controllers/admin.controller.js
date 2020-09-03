a3c79057f50d181e7fec2f5a44c1311966a6ba47
Ji Kyu-Bin <jikyubin72@gmail.com>
2020-09-04 01:28:54 +0900

    코드라인 변경
    catch가 이상한 곳에 가있어서 바꿨습니다.

--- router/controllers/admin.controller.js
+++ router/controllers/admin.controller.js
@@ -31,9 +31,9 @@ module.exports.createToken = function (req, res, next) {
                     });
                 }
             })
-                .catch((err) => {
-                    console.log(err);
-                    res.status(500).json({ status: "error" });
-                });
         })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json({ status: "error" });
+        });
 }
