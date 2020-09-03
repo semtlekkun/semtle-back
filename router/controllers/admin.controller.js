601f894e040e9e5ef075be8fdcde748b28c52f64
Ji Kyu-Bin <jikyubin72@gmail.com>
2020-09-04 01:22:27 +0900

    관리자 해시값 변경

--- router/controllers/admin.controller.js
+++ router/controllers/admin.controller.js
@@ -1,33 +1,39 @@
 const Admin = require("../../schemas/admin");
 const jwt = require("jsonwebtoken");
 const secretKey = require('../../config/jwt');
+const bcrypt = require('bcrypt');
+const saltRounds = require("../../config/hash").saltRounds;
 
-module.exports.createToken = function(req,res,next){
-    Admin.find(req.body)
-    .then((admin)=>{
-        if(admin.length){
-            const token = jwt.sign({
-                id:admin[0]._id,
-                isAdmin:true
-            },
-            secretKey.secret,
-            {
-                expiresIn:'6h'
-            });
-            res.status(200).json({
-                status:'success',
-                token:token,
-                admin:true
-            });
-        }
-        else{
-            res.status(400).json({
-                status:"wrong"
-            });
-        }
-    })
-    .catch((err)=>{
-        console.log(err);
-        res.status(500).json({status:"error"});
-    });
+module.exports.createToken = function (req, res, next) {
+    Admin.findOne({ _id: req.body._id }, { pw: true })
+        .then((admin) => {
+            if (admin == null) res.status(400).json({ status: "wrong" });
+            bcrypt.compare(req.body.pw, admin.pw, function (err, result) {
+                if (err) res.status(500).json({ status: "error" });
+                if (result) {
+                    const token = jwt.sign({
+                        id: admin._id,
+                        isAdmin: true
+                    },
+                        secretKey.secret,
+                        {
+                            expiresIn: '6h'
+                        });
+                    res.status(200).json({
+                        status: 'success',
+                        token: token,
+                        admin: true
+                    });
+                }
+                else {
+                    res.status(400).json({
+                        status: "wrong"
+                    });
+                }
+            })
+                .catch((err) => {
+                    console.log(err);
+                    res.status(500).json({ status: "error" });
+                });
+        })
 }
