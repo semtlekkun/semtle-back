716a15f940fa2b75602bdf5088639f7f621b273a
yooncastle <dbstjd427@gmail.com>
2020-09-08 21:26:51 +0900

    createCipher >> createCipheriv

--- schemas/student.js
+++ schemas/student.js
@@ -33,11 +33,17 @@ const studentSchema = new Schema({
 
 studentSchema.pre('save', function (next) {
     const user = this;
-    //phoneNum Encoding
-    const cipher = crypto.createCipher('aes-256-cbc', 'yooncastle');
-    let result = cipher.update(user.phoneNum, 'utf8', 'base64'); // 'HbMtmFdroLU0arLpMflQ'
-    result += cipher.final('base64'); // 'HbMtmFdroLU0arLpMflQYtt8xEf4lrPn5tX5k+a8Nzw='
-    user.phoneNum = result;
+    // //phoneNum Encoding
+    // const cipher = crypto.createCipher('aes-256-cbc', 'yooncastle');
+    // let result = cipher.update(user.phoneNum, 'utf8', 'base64'); // 'HbMtmFdroLU0arLpMflQ'
+    // result += cipher.final('base64'); // 'HbMtmFdroLU0arLpMflQYtt8xEf4lrPn5tX5k+a8Nzw='
+    // user.phoneNum = result;
+
+    const cipher = crypto.createCipheriv('aes-256-cbc',
+        new Buffer('yooncastle'), new Buffer('20161313'));
+    var crypted = cipher.update(user.phoneNum, 'utf8', 'hex');
+    crypted += cipher.final('hex')
+    user.phoneNum = crypted;
 
     //pw Hashing
     bcrypt.hash(user.pw, saltRounds, function (err, hash) {
