065a1a71188ff6441537c2fc1694a56dec60bd6c
yooncastle <dbstjd427@gmail.com>
2020-09-08 12:27:15 +0900

    phoneNumber encoding

--- schemas/student.js
+++ schemas/student.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 const saltRounds = require("../config/hash").saltRounds;
 const bcrypt = require("bcrypt");
+const crypto = require("crypto");
 const studentSchema = new Schema({
     _id: {
         type: String,
@@ -32,6 +33,13 @@ const studentSchema = new Schema({
 
 studentSchema.pre('save', function (next) {
     const user = this;
+    //phoneNum Encoding
+    const cipher = crypto.createCipher('aes-256-cbc', 'yooncastle');
+    let result = cipher.update(user.phoneNum, 'utf8', 'base64'); // 'HbMtmFdroLU0arLpMflQ'
+    result += cipher.final('base64'); // 'HbMtmFdroLU0arLpMflQYtt8xEf4lrPn5tX5k+a8Nzw='
+    user.phoneNum = result;
+
+    //pw Hashing
     bcrypt.hash(user.pw, saltRounds, function (err, hash) {
         if (err) {
             console.log(err);
